3b262dbffb89e1fac98e2d5df9b484be1a409179
Blax-KaTo <79809212+Blax-KaTo@users.noreply.github.com>
2023-06-09 03:40:03 -0600

    menu.php

--- menu.php
+++ menu.php
@@ -2,27 +2,25 @@
 session_start();
 include('config.php');
 
-// Check if the form is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve selected products and quantities
-    if (isset($_POST['selectedProducts'])) {
-        $selectedProducts = $_POST['selectedProducts'];
-        $quantities = $_POST['quantity'];
-
-        // Store the selected products and quantities in session
-        $_SESSION['selectedProducts'] = $selectedProducts;
-        $_SESSION['quantities'] = $quantities;
-
-        // Redirect to ingredient.php
-        header("Location: ingredient.php");
-        exit();
-    }
-}
-
-// Retrieve all products from the database
-$productQuery = "SELECT * FROM products";
+// Retrieve the list of available products from the database
+$productQuery = "SELECT * FROM products WHERE active = 1";
 $productResult = mysqli_query($conn, $productQuery);
 $products = mysqli_fetch_all($productResult, MYSQLI_ASSOC);
+
+// Handle form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the selected products and quantities
+    $selectedProducts = $_POST['selectedProducts'];
+    $quantities = $_POST['quantities'];
+
+    // Store the selected products and quantities in session variables
+    $_SESSION['selectedProducts'] = $selectedProducts;
+    $_SESSION['quantities'] = $quantities;
+
+    // Redirect to the ingredient selection page
+    header("Location: ingredient.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -37,22 +35,25 @@ $products = mysqli_fetch_all($productResult, MYSQLI_ASSOC);
 <body>
     <div class="main-content">
         <h1>Menu</h1>
-
-        <!-- Display the products and quantity form -->
-<form action="menu.php" method="post">
-    <?php foreach ($products as $product) : ?>
-        <div>
-            <input type="checkbox" name="selectedProducts[]" value="<?php echo $product['id']; ?>">
-            <?php echo $product['name']; ?>
-            <input type="number" name="quantity[<?php echo $product['id']; ?>]" min="1" value="1">
-        </div>
-    <?php endforeach; ?>
-    <button type="submit">Submit</button>
-</form>
-
-        <footer><p><b>Vwok</b> CORP.</p></footer>
+        <form action="" method="post">
+            <?php foreach ($products as $product) : ?>
+                <div class="product-card">
+                    <div class="product-image">
+                        <img src="<?php echo $product['image']; ?>" alt="<?php echo $product['name']; ?>">
+                    </div>
+                    <div class="product-details">
+                        <h3><?php echo $product['name']; ?></h3>
+                        <p><?php echo $product['description']; ?></p>
+                        <label for="quantity_<?php echo $product['id']; ?>">Quantity:</label>
+                        <input type="number" name="quantities[<?php echo $product['id']; ?>]" id="quantity_<?php echo $product['id']; ?>" value="1" min="1">
+                        <input type="checkbox" name="selectedProducts[]" value="<?php echo $product['id']; ?>">
+                        Select
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <button type="submit">Proceed to Ingredients</button>
+        </form>
     </div>
-
     <script src="script/menu.js"></script>
 </body>
 </html>
